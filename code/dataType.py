659f239d4027449d1cb38ed9c2178caa7eaa5701
Robert Micatka <prometheus672@gmail.com>
2014-05-05 23:51:26 -0400

    first commit

---
+++ code/dataType.py
@@ -0,0 +1,17 @@
+'''
+Datatypes for Moirai
+
+Robert Micatka
+May 5th, 2014
+'''
+
+from collections import namedtuple
+
+#for now just have dailyStockType, will add more later
+dailyStockType = namedtuple('dailyStockType', 'data, open, high, low, close, volume, adjClose')
+
+
+#add in all available data for complete database entries, can always ignore it later.
+
+
+
